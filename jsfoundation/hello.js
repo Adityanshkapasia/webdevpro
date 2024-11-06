0a9ae644df3e9e09e67ab5f112899a033679be93
Adityansh Kapasia <adityansh7@gmail.com>
2024-11-06 11:32:54 +0530

    v1

---
+++ jsfoundation/hello.js
@@ -0,0 +1,97 @@
+// console.log("Hello World");
+// console.log();
+// console.log();
+
+// let teaflavpur = ["Tea1", "Tea2", "Tea3"];
+
+// let firsttea = teaflavpur[0];
+
+// console.log(firsttea);
+
+//challenge 2
+
+// let cities = ["london","tokyo","delhi","shangai"];
+
+// let favcity = cities[2];
+
+//challenge 3
+
+// let teaflavpur = ["Tea1", "Tea2", "Tea3", "Tea4"];
+
+// teaflavpur[2] = "Tea5";
+
+// console.log(teaflavpur[2]);
+
+//challenge 4
+
+// let citiesvisted = ["london", "tokyo", "paris", "sydney"];
+
+// citiesvisted[citiesvisted.length] = "nyc";
+
+// citiesvisted.push("nyc");
+
+// console.log(citiesvisted);
+
+// chal 5
+
+// let citiesvis = ["london", "tokyo", "paris", "sydney", "delhi"];
+
+// console.log(citiesvis);
+
+// let lastcity = citiesvis[citiesvis.length - 1];
+
+// console.log(lastcity);
+
+// citiesvis.pop();
+
+// console.log("Popped");
+
+// console.log(citiesvis);
+
+// chal 6 - softcopy
+
+// let arr1 = ["ele1", "ele2", "ele3"];
+
+// let arr2 = arr1;
+
+// arr1.pop();
+
+// console.log(arr2);
+
+//chal 7 hard copy
+
+// let arr1 = ["ele1", "ele2", "ele3", "ele4"];
+
+// let arr2 = [...arr1];
+
+// arr2.pop();
+
+// arr1.push("ele5");
+
+// console.log(arr1);
+
+// console.log(arr2);
+
+// chal 8 cocat
+
+// let arr1 = ["ele1", "ele2", "ele3", "ele4"];
+
+// let arr2 = ["ele5", "ele6"];
+
+// let arr3 = arr1.concat(arr2);
+
+// console.log(arr3);
+
+// chal 9 length of array
+
+// let arr1 = ["ele1", "ele2", "ele3"];
+
+// let arrlen = arr1.length;
+
+// console.log(arrlen);
+
+//chal 10 chek if ele3 in arr
+
+// let arr1 = ["ele1","ele2","ele3","ele4"];
+
+// let isele3inlist = false;
